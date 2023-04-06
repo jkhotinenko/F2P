829d19ecdc436e619c1cdba8bb6a7d3b39f097e4
Yaroslav Khotinenko <jkhotinenko@gmail.com>
2023-04-06 13:18:33 +0300

    init

---
+++ src/main/java/ua/com/alxs/f2p/F2PApplication.java
@@ -0,0 +1,13 @@
+package ua.com.alxs.f2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F2PApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(F2PApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/alxs/f2p/F2PApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.alxs.f2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F2PApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
